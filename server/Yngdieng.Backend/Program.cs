bfa7c7b30f68760414ff96ff53ccdc864401d3ce
Radium Zheng <ztl8702@users.noreply.github.com>
2019-11-20 00:42:34 +1100

    save

---
+++ server/Yngdieng.Backend/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Yngdieng.Backend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ server/Yngdieng.Indexer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Yngdieng.Indexer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
