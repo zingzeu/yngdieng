2d65fa93df1c14ff3725abb95f88277ca084b99a
Radium Zheng <ztl8702@users.noreply.github.com>
2019-11-23 06:08:54 +1100

    ok

--- test_dotnet/Program.cs
+++ test_dotnet/Program.cs
@@ -1,9 +1,24 @@
 using System;
+using Yngdieng.Protos;
+using Grpc.Core;
+using System.Threading.Tasks;
 
 namespace test_dotnet {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("It works");
+            var a = new Document {
+                Tone = Tone.DownLevel,
+                Initial = Initial.B
+            };
+            Console.WriteLine(a);
+        }
+    }
+
+    public class YngdiengServiceImpl:YngdiengService.YngdiengServiceBase {
+        public override Task<SearchResponse> GetSearch(SearchRequest request, ServerCallContext context) {
+            Console.WriteLine("A search request"); 
+            return Task.FromResult(new SearchResponse());
         }
     }
 }
