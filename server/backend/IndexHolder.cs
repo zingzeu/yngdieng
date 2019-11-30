ab7e406e5873e05381ecefeb76e57b513bcdc0d2
Radium Zheng <ztl8702@users.noreply.github.com>
2019-11-30 21:33:59 +1100

    works: requests

---
+++ server/backend/IndexHolder.cs
@@ -0,0 +1,40 @@
+using System.IO;
+using System;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using Yngdieng.Protos;
+
+namespace Yngdieng.Backend
+{
+    public interface IIndexHolder
+    {
+        YngdiengIndex GetIndex();
+    }
+
+    public sealed class IndexHolder : IIndexHolder
+    {
+        private readonly ILogger<IndexHolder> _logger;
+
+        private YngdiengIndex index;
+
+        public IndexHolder(IConfiguration config, ILogger<IndexHolder> logger)
+        {
+            var indexFilePath = config["IndexFile"];
+            _logger = logger;
+
+            _logger.LogInformation($"Loading index from {Path.GetFullPath(indexFilePath)}");
+
+            using (var input = File.OpenRead(indexFilePath))
+            {
+                index = YngdiengIndex.Parser.ParseFrom(input);
+            }
+
+            _logger.LogInformation($"{index.Documents.Count} documents loaded.");
+        }
+
+        public YngdiengIndex GetIndex()
+        {
+            return index;
+        }
+    }
+}
--- server/backend/Services/YngdiengService.cs
+++ server/backend/Services/YngdiengService.cs
@@ -2,14 +2,125 @@ using System;
 using System.Threading.Tasks;
 using Yngdieng.Protos;
 using Grpc.Core;
-
-namespace Yngdieng.Backend.Services {
-public class YngdiengService : Yngdieng.Protos.YngdiengService.YngdiengServiceBase
+using Microsoft.Extensions.Logging;
+using System.Linq;
+namespace Yngdieng.Backend.Services
+{
+    public class YngdiengService : Yngdieng.Protos.YngdiengService.YngdiengServiceBase
     {
+        private readonly ILogger<YngdiengService> _logger;
+        private readonly IIndexHolder _indexHolder;
+
+        public YngdiengService(ILogger<YngdiengService> logger, IIndexHolder indexHolder)
+        {
+            _logger = logger;
+            _indexHolder = indexHolder;
+        }
         public override Task<SearchResponse> GetSearch(SearchRequest request, ServerCallContext context)
         {
-            Console.WriteLine("A search request");
-            return Task.FromResult(new SearchResponse());
+            _logger.LogInformation("A search request" + request.ToString());
+            switch (request.Query.QueryCase)
+            {
+                case Query.QueryOneofCase.PhonologyQuery:
+                    {
+                        Initial initial = request.Query.PhonologyQuery.Initial;
+                        Final final = request.Query.PhonologyQuery.Final;
+                        Tone tone = request.Query.PhonologyQuery.Tone;
+                        if (initial == Initial.Unspecified &&
+                            final == Final.Unspecified &&
+                            tone == Tone.Unspecified)
+                        {
+                            throw new Exception("Cannot all be unspecified");
+                        }
+                        var documents = _indexHolder.GetIndex().Documents.Where(_ => true);
+                        if (initial != Initial.Unspecified)
+                        {
+                            documents = documents.Where(d => d.Initial == initial);
+                        }
+                        if (final != Final.Unspecified)
+                        {
+                            documents = documents.Where(d => d.Final == final);
+                        }
+                        if (tone != Tone.Unspecified)
+                        {
+                            documents = documents.Where(d => d.Tone == tone);
+                        }
+                        var matchedDocuments = documents.OrderBy(d =>
+                        {
+                            return GetHanzi(d.HanziCanonical);
+                        }).ThenBy(d => d.Final)
+                                .ThenBy(d => d.Initial)
+                                .ThenBy(d => d.Tone)
+                                .ToList();
+                        var response = new SearchResponse();
+                        response.Documents.Add(matchedDocuments);
+                        return Task.FromResult(response);
+                    }
+                case Query.QueryOneofCase.HanziQuery:
+                    {
+                        string query = request.Query.HanziQuery;
+                        if (query == null)
+                        {
+                            throw new Exception("Cannot all be null");
+                        }
+                        var matchedDocuments = _indexHolder.GetIndex().Documents
+                            .Where(d =>
+                                GetHanzi(d.HanziCanonical) == query
+                                || d.HanziAlternatives.Where(
+                                    r => GetHanzi(r) == query).Count() > 0
+                            )
+                            .OrderBy(d => GetHanzi(d.HanziCanonical))
+                                .ThenBy(d => d.Final)
+                                .ThenBy(d => d.Initial)
+                                .ThenBy(d => d.Tone)
+                            .ToList();
+                        var response = new SearchResponse();
+                        response.Documents.Add(matchedDocuments);
+                        return Task.FromResult(response);
+                    }
+                default:
+                    throw new Exception("Not implemented");
+            }
+        }
+
+        public static string GetHanzi(Hanzi h)
+        {
+            return h.HanziCase == Hanzi.HanziOneofCase.Regular
+                        ? h.Regular
+                        : h.Ids;
+        }
+        public override Task<GetDocumentResponse> GetDocument(GetDocumentRequest request, ServerCallContext context)
+        {
+            _logger.LogInformation("GetDocumentRequest: " + request.ToString());
+            switch (request.DocumentIdCase)
+            {
+                case GetDocumentRequest.DocumentIdOneofCase.CiklinId:
+                    {
+                        var ciklinId = request.CiklinId;
+                        var matchedDocument = _indexHolder
+                            .GetIndex()
+                            .Documents
+                            .Where(document =>
+                                (document as Document).CiklinId == ciklinId)
+                            .FirstOrDefault();
+                        return Task.FromResult(new GetDocumentResponse()
+                        {
+                            Document = matchedDocument
+                        });
+                    }
+                case GetDocumentRequest.DocumentIdOneofCase.DfdId:
+                    {
+                        var dfdId = request.DfdId;
+                        var matchedDocument = _indexHolder.GetIndex().Documents.Where(document => (document as Document).DfdId == dfdId).FirstOrDefault();
+                        return Task.FromResult(new GetDocumentResponse()
+                        {
+                            Document = matchedDocument
+                        });
+                    }
+                default:
+                    throw new Exception("Bad request");
+            }
+            return Task.FromResult(new GetDocumentResponse());
         }
     }
 }
