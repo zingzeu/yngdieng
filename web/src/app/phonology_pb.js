8b4eadafa668e547dee23b919fbb140d3d34f6bb
Radium Zheng <ztl8702@users.noreply.github.com>
2019-11-30 22:01:05 +1100

    grpc js

---
+++ web/src/app/phonology_pb.js
@@ -0,0 +1,94 @@
+// source: phonology.proto
+/**
+ * @fileoverview
+ * @enhanceable
+ * @suppress {messageConventions} JS Compiler reports an error if a variable or
+ *     field starts with 'MSG_' and isn't a translatable message.
+ * @public
+ */
+// GENERATED CODE -- DO NOT EDIT!
+
+var jspb = require('google-protobuf');
+var goog = jspb;
+var global = Function('return this')();
+
+goog.exportSymbol('proto.yngdieng.Final', null, global);
+goog.exportSymbol('proto.yngdieng.Initial', null, global);
+goog.exportSymbol('proto.yngdieng.Tone', null, global);
+/**
+ * @enum {number}
+ */
+proto.yngdieng.Final = {
+  FINAL_UNSPECIFIED: 0,
+  UNG: 1,
+  UA: 2,
+  YONG: 3,
+  IU: 4,
+  ANG: 5,
+  AI: 6,
+  A: 7,
+  ING: 8,
+  UANG: 9,
+  O: 10,
+  Y: 11,
+  UOI: 12,
+  U: 13,
+  EING: 14,
+  UONG: 15,
+  UI: 16,
+  IEU: 17,
+  YNG: 18,
+  ONG: 19,
+  I: 20,
+  OENG: 21,
+  AU: 22,
+  UO: 23,
+  E: 24,
+  IO: 25,
+  IE: 26,
+  IANG: 27,
+  OEY: 28,
+  OE: 29,
+  IENG: 30,
+  IA: 31,
+  UAI: 32,
+  EU: 33
+};
+
+/**
+ * @enum {number}
+ */
+proto.yngdieng.Initial = {
+  INITIAL_UNSPECIFIED: 0,
+  L: 1,
+  B: 2,
+  G: 3,
+  K: 4,
+  D: 5,
+  P: 6,
+  T: 7,
+  Z: 8,
+  N: 9,
+  S: 10,
+  NONE: 11,
+  M: 12,
+  NG: 13,
+  C: 14,
+  H: 15
+};
+
+/**
+ * @enum {number}
+ */
+proto.yngdieng.Tone = {
+  TONE_UNSPECIFIED: 0,
+  UP_LEVEL: 1,
+  UP_UP: 2,
+  UP_FALLING: 3,
+  UP_ABRUPT: 4,
+  DOWN_LEVEL: 5,
+  DOWN_FALLING: 6,
+  DOWN_ABRUPT: 7
+};
+
+goog.object.extend(exports, proto.yngdieng);
