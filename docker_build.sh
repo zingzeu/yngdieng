6fd7cdc7c4bd50b1e4bd443e75ff924589802409
Radium Zheng <ztl8702@users.noreply.github.com>
2020-01-23 03:55:17 +1100

    Docker: yngdieng-web

--- docker_build.sh
+++ docker_build.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+mkdir -p ./.build/yngdieng-web
+bazel build //web/src:prodapp
+cp -r ./dist/bin/web/src/prodapp ./.build/yngdieng-web/
+cp -r ./docker/yngdieng-web/nginx.conf ./.build/yngdieng-web/
+
+docker build -f docker/yngdieng-web/Dockerfile -t ztl8702/yngdieng-web ./.build/yngdieng-web
+
+
 docker build -f docker/yngdieng-backend/Dockerfile -t ztl8702/yngdieng-backend ./server
