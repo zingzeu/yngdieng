8cc938a6e1c5626725f723f3def6f565271901da
Radium Zheng <ztl8702@users.noreply.github.com>
2020-02-29 16:17:46 +0800

    Save

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -3,6 +3,7 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import {MatChipsModule} from '@angular/material/chips';
+import {MatExpansionModule} from '@angular/material/expansion';
 import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
 import {MatPaginatorModule} from '@angular/material/paginator';
@@ -52,6 +53,7 @@ import {YngdiengBackendService} from './yngdieng-backend.service';
     MatIconModule,
     MatInputModule,
     MatToolbarModule,
+    MatExpansionModule,
     MatButtonModule,
     MatCardModule,
     MatSlideToggleModule,
