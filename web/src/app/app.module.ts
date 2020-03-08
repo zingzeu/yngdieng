ede72175f9009a4360d365e75fee8f7c5539dddc
Radium Zheng <ztl8702@users.noreply.github.com>
2020-03-08 20:26:21 +0800

    save

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -4,6 +4,7 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import {MatChipsModule} from '@angular/material/chips';
 import {MatExpansionModule} from '@angular/material/expansion';
+import {MatGridListModule} from '@angular/material/grid-list';
 import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
 import {MatListModule} from '@angular/material/list';
@@ -67,6 +68,7 @@ import {YngdiengBackendService} from './yngdieng-backend.service';
     MatSlideToggleModule,
     MatSidenavModule,
     MatPaginatorModule,
+    MatGridListModule,
     MatListModule,
     FormsModule,
     ReactiveFormsModule
