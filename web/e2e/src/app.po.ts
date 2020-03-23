676fb3a8dbfb36b5c14141ef2e8d116ff3a80d29
Radium <radium@mindong.asia>
2020-03-23 12:19:34 +0800

    fix e2e test

--- web/e2e/src/app.po.ts
+++ web/e2e/src/app.po.ts
@@ -1,11 +1,17 @@
-import {browser, by, element} from 'protractor';
+import {browser, by, element, ElementFinder} from 'protractor';
 
 export class AppPage {
+  logo: ElementFinder = element(by.css('app-root .logo'));
+
   navigateTo() {
     return browser.get(browser.baseUrl) as Promise<any>;
   }
 
-  getLogoText() {
-    return element(by.css('app-root .logo')).getText() as Promise<string>;
+  getLogoTag() {
+    return this.logo.getTagName() as Promise<string>;
+  }
+
+  getLogoSrc() {
+    return this.logo.getAttribute('src') as Promise<string>;
   }
 }
