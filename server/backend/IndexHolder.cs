5077488009cab3df0627d9280783edaa16acc168
Radium Zheng <ztl8702@users.noreply.github.com>
2020-03-24 04:54:27 +0800

    load index on start

--- server/backend/IndexHolder.cs
+++ server/backend/IndexHolder.cs
@@ -1,40 +1,25 @@
-using System.IO;
-using System;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Configuration;
 using Yngdieng.Protos;
-using Yngdieng.Backend.HealthChecks;
 
 namespace Yngdieng.Backend
 {
   public interface IIndexHolder
   {
-    YngdiengIndex GetIndex();
+      void StoreIndex(YngdiengIndex index);
+      YngdiengIndex GetIndex();
   }
 
   public sealed class IndexHolder : IIndexHolder
   {
-    private readonly ILogger<IndexHolder> _logger;
-    private readonly IndexHealthCheck _indexHealthCheck;
-    private YngdiengIndex index;
+      private YngdiengIndex index = new YngdiengIndex();
 
-    public IndexHolder(IConfiguration config,
-                       ILogger<IndexHolder> logger,
-                       IndexHealthCheck indexHealthCheck)
-    {
-      var indexFilePath = config["IndexFile"];
-      _logger = logger;
-      this._indexHealthCheck = indexHealthCheck;
-      _logger.LogInformation($"Loading index from {Path.GetFullPath(indexFilePath)}");
-
-      using (var input = File.OpenRead(indexFilePath))
+      public IndexHolder()
       {
-        index = YngdiengIndex.Parser.ParseFrom(input);
       }
 
-      _indexHealthCheck.IndexLoaded = true;
-      _logger.LogInformation($"{index.Documents.Count} documents loaded.");
-    }
+      public void StoreIndex(YngdiengIndex index)
+      {
+          this.index = index;
+      }
 
     public YngdiengIndex GetIndex()
     {
--- server/backend/Startup.cs
+++ server/backend/Startup.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -24,6 +20,7 @@ namespace Yngdieng.Backend
         services.AddHealthChecks().AddCheck<IndexHealthCheck>("index_file_loading");
         services.AddGrpc();
         services.AddSingleton<IIndexHolder, IndexHolder>();
+        services.AddHostedService<IndexLoaderBackgroundService>();
         services.AddSingleton<ISearchCache, InMemorySearchCache>();
     }
 
