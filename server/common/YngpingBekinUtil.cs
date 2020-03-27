e5dadf1390a08f798822256edd514da679cc6989
Radium <radium@mindong.asia>
2020-03-28 06:29:20 +0800

    save

--- server/common/YngpingBekinUtil.cs
+++ server/common/YngpingBekinUtil.cs
@@ -130,7 +130,7 @@ namespace Yngdieng.Common
             case Final.Uong:
               return "uong";
             case Final.Oeng:
-                return "eong";
+              return "eong";
             case Final.Eing:
               return "eng";
             case Final.Ieng:
