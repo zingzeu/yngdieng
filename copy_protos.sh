c33201ba0fc522b22e77ff7dd9d8f8eb2dea4af6
Radium Zheng <ztl8702@users.noreply.github.com>
2020-04-01 02:16:03 +0800

    add explanations proto

--- copy_protos.sh
+++ copy_protos.sh
@@ -2,4 +2,7 @@
 
 # Copy protos to csharp build project
 
+mkdir -p ./server/protos/shared/
+mkdir -p ./server/protos/shared/zingzeudata/
 cp ./shared/*.proto ./server/protos/shared/
+cp ./shared/zingzeudata/*.proto ./server/protos/shared/zingzeudata/
