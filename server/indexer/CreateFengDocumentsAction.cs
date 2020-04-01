0dc00bf40e131c549d241fc57b5c618db88a1fa5
Radium Zheng <ztl8702@users.noreply.github.com>
2020-04-02 03:54:35 +0800

    save

--- server/indexer/CreateFengDocumentsAction.cs
+++ server/indexer/CreateFengDocumentsAction.cs
@@ -1,3 +1,4 @@
+extern alias zingzeudata;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -6,183 +7,86 @@ using Yngdieng.Protos;
 using Yngdieng.Common;
 using System.Net.Http;
 using System.Text;
+using ZingzeuData.Parser;
+using ZingzeuData.Models;
+using Google.Protobuf;
 
 namespace Yngdieng.Indexer
 {
-  public sealed class CreateFengDocumentsAction
-  {
-
-    private static readonly string OpenCCDaemon = "http://localhost:8081";
-    private static readonly HttpClient client = new HttpClient();
-    private readonly string mergedPath;
-    private readonly string outputFolder;
-
-    public CreateFengDocumentsAction(string mergedPath, string outputFolder)
+    public sealed class CreateFengDocumentsAction
     {
-      this.mergedPath = mergedPath;
-      this.outputFolder = outputFolder;
-    }
 
-    public IEnumerable<FengDocument> Run()
-    {
-      var jsonOutput = new List<string>();
-      var documents = new List<FengDocument>();
+        private static readonly string OpenCCDaemon = "http://localhost:8081";
+        private static readonly HttpClient client = new HttpClient();
+        private readonly string mergedPath;
+        private readonly string outputFolder;
 
-      var docs = LoadFengRows(mergedPath).Select(f =>
-      {
-        var tmp = new FengDocument
+        public CreateFengDocumentsAction(string mergedPath, string outputFolder)
         {
-          Id = $"p{f.PageNumber}_{f.LineNumber}",
-          HanziCanonical = f.KanjiClean,
-          YngpingCanonical = f.Pron,
-          Explanation = f.Explanation,
-          ExplanationHans = Simplify(f.Explanation),
-          Source = new FengDocument.Types.SourceInfo
-          {
-            PageNumber = f.PageNumber,
-            LineNumber = f.LineNumber
-          },
-        };
-        tmp.HanziMatchable.Add(f.KanjiClean);
-        tmp.HanziMatchable.Add(Simplify(f.KanjiClean));
-        tmp.YngpingPermutations.Add(f.Pron);
-        tmp.YngpingPermutations.AddRange(YngpingVariantsUtil.GenerateYngpingVariants(f.Pron));
-        return tmp;
-      });
-
-      documents.AddRange(docs);
-      jsonOutput.AddRange(docs.Select(proto => proto.ToString()));
-      File.WriteAllLines(Path.Combine(outputFolder, "feng_index_debug.txt"), jsonOutput);
-      return documents;
-    }
-
-    public static FengRow[] LoadFengRows(string path)
-    {
-      var mlines = File.ReadAllLines(path);
-      var mEntries = mlines
-          .Select((x, index) => new
-          {
-            Line = x,
-            Index = index + 1
-          })
-          .Where(x => !x.Line.StartsWith("==="))
-          .Select(x => new
-          {
-            x.Index,
-            Tokens = x.Line.Split('\t')
-          })
-          .Where(x => x.Tokens.Length > 1)
-          .Select(x => new FengRow
-          {
-            GlobalLineNumber = x.Index,
-            Pron = x.Tokens[2].Trim(),
-            KanjiClean = CleanKanji(GetKanji(x.Tokens[0])),
-            Explanation = ReplaceAllBraces(String.Join(" ", x.Tokens[3..^0]))
-          })
-          .ToArray();
-
-      var mPageBreaks = mlines
-          .Select((x, index) => new
-          {
-            Line = x,
-            Index = index + 1
-          })
-          .Where(x => x.Line.StartsWith("==="))
-          .Select(x => new
-          {
-            LineNumber = x.Index,
-            PageNumber = int.Parse(x.Line.Substring(x.Line.IndexOf("PAGE=", StringComparison.Ordinal) + 5))
-          }).ToArray();
-
-      // Assign page number
-      var numInPage = 1;
-      for (int i = 0, j = 0; i < mEntries.Length; ++i)
-      {
-        while (j < mPageBreaks.Length - 1 && mPageBreaks[j + 1].LineNumber < mEntries[i].GlobalLineNumber) { ++j; numInPage = 1; }
-        mEntries[i].PageNumber = mPageBreaks[j].PageNumber;
-        mEntries[i].LineNumber = numInPage++;
-      }
-      return mEntries;
-    }
-
-    public static string GetKanji(string input)
-    {
-      var start = input.IndexOf("【");
-      var end = input.IndexOf("】");
-      if (end <= start)
-      {
-        throw new Exception("GetKanji Error " + input);
-      }
-      return input.Substring(start + 1, end - start - 1);
-    }
-
-    private static string CleanKanji(string input)
-    {
-      var s = input.Trim();
-      while (true)
-      {
-        var tmp = ReplaceBracesOnce(s);
-        if (tmp == s) break;
-        s = tmp;
-      }
-      return s.Replace("*", "");
-    }
-
-    private static string ReplaceBracesOnce(string input)
-    {
-      var bStart = input.IndexOf("{");
-      var bEnd = input.IndexOf("}");
-      if (bStart == -1 || bEnd == -1)
-      {
-        return input;
-      }
-      var bComma = input.Substring(bStart).IndexOf(",") + bStart;
-      if (!(bStart < bComma && bComma < bEnd))
-      {
-        throw new Exception($"Error input: {input}. BraceStart={bStart} Comma={bComma} BraceEnd={bEnd}");
-      }
-      var corrected = input.Substring(bComma + 1, bEnd - bComma - 1);
-      return input.Substring(0, bStart) + corrected + input.Substring(bEnd + 1);
-    }
-
-    private static string ReplaceAllBraces(string input)
-    {
-      var tmp = input;
-      while (true)
-      {
-        var replaced = ReplaceBracesOnce(tmp);
-        if (tmp == replaced)
-        {
-          return tmp;
+            this.mergedPath = mergedPath;
+            this.outputFolder = outputFolder;
         }
-        tmp = replaced;
-      }
+
+        public IEnumerable<FengDocument> Run()
+        {
+            var jsonOutput = new List<string>();
+            var documents = new List<FengDocument>();
+
+            var docs =
+                zingzeudata.ZingzeuData.Parser.ParseFeng.LoadFengRows(mergedPath).Select(f => {
+                    var cleanExplanation =
+                        zingzeudata.ZingzeuData.Shared.StringHelpers.ReplaceAllBraces(
+                            f.ExplanationRaw);
+                    var tmp = new FengDocument{
+                        Id = $"p{f.PageNumber}_{f.LineNumber}",
+                        HanziCanonical = f.HanziClean,
+                        YngpingCanonical = f.Pron,
+                        Explanation = cleanExplanation,
+                        ExplanationHans = Simplify(cleanExplanation),
+                        ExplanationStructured = SafeParseExplanation(cleanExplanation),
+                        Source = new FengDocument.Types.SourceInfo{PageNumber = f.PageNumber,
+                                                                   LineNumber = f.LineNumber},
+                    };
+                    tmp.HanziMatchable.Add(f.HanziClean);
+                    tmp.HanziMatchable.Add(Simplify(f.HanziClean));
+                    tmp.YngpingPermutations.Add(f.Pron);
+                    tmp.YngpingPermutations.AddRange(
+                        YngpingVariantsUtil.GenerateYngpingVariants(f.Pron));
+                    return tmp;
+                });
+
+            documents.AddRange(docs);
+            jsonOutput.AddRange(docs.Select(proto => proto.ToString()));
+            File.WriteAllLines(Path.Combine(outputFolder, "feng_index_debug.txt"), jsonOutput);
+            return documents;
+        }
+
+        private static string Simplify(string traditional)
+        {
+            return client
+                .PostAsync(OpenCCDaemon, new ByteArrayContent(Encoding.UTF8.GetBytes(traditional)))
+                .Result.Content.ReadAsStringAsync()
+                .Result;
+        }
+
+        private static Explanation SafeParseExplanation(string rawExplanation)
+        {
+            try
+            {
+                return ConvertExplanation(FengExplanationParser.Parse(rawExplanation));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"{e.Message} {e.StackTrace}");
+                return null;
+            }
+        }
+
+        private static Explanation ConvertExplanation(
+            zingzeudata.ZingzeuData.Models.Explanation explanation)
+        {
+            return Explanation.Parser.ParseFrom(explanation.ToByteArray());
+        }
     }
 
-    private static string Simplify(string traditional)
-    {
-      return client.PostAsync(OpenCCDaemon, new ByteArrayContent(Encoding.UTF8.GetBytes(traditional))).Result.Content.ReadAsStringAsync().Result;
-    }
-  }
-
-
-  /// <summary>
-  /// Model for each row in feng.txt.
-  /// </summary>
-  public class FengRow
-  {
-    /// <summary>
-    /// Line number in feng.txt.
-    /// </summary>
-    public int GlobalLineNumber { get; set; }
-    /// <summary>
-    /// Line number in page
-    /// </summary>
-    public int LineNumber { get; set; }
-    public int PageNumber { get; set; }
-    public string Pron { get; set; }
-    public string KanjiClean { get; set; }
-
-    public string Explanation { get; set; }
-  }
 }
