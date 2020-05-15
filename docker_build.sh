d82f9ffd936990095a9176e1839ad1948da1ddf5
Radium Zheng <ztl8702@users.noreply.github.com>
2020-05-16 01:49:39 +0800

    save

--- docker_build.sh
+++ docker_build.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-mkdir -p ./.build/yngdieng-web
+# Prod web image
+mkdir -p ./.build/yngdieng-web-prod
 bazel build //web/src:prodapp
-cp -r ./dist/bin/web/src/prodapp ./.build/yngdieng-web/
-cp -r ./docker/yngdieng-web/nginx.conf ./.build/yngdieng-web/
+cp -r ./dist/bin/web/src/prodapp ./.build/yngdieng-web-prod/
+cp -r ./docker/yngdieng-web-prod/nginx.conf ./.build/yngdieng-web-prod/
+docker build -f docker/yngdieng-web-prod/Dockerfile -t ztl8702/yngdieng-web-prod ./.build/yngdieng-web-prod
 
-docker build -f docker/yngdieng-web/Dockerfile -t ztl8702/yngdieng-web ./.build/yngdieng-web
+# Dev web image
+#mkdir -p ./.build/yngdieng-web-dev
+#bazel build //web/src:devapp_release
+#cp -r ./dist/bin/web/src/devapp_release ./.build/yngdieng-web-dev/
+#cp -r ./docker/yngdieng-web-dev/nginx.conf ./.build/yngdieng-web-dev/nginx.conf
+#docker build -f docker/yngdieng-web-dev/Dockerfile -t ztl8702/yngdieng-web-dev ./.build/yngdieng-web-dev
 
-
+# Server image (just one for now)
 docker build -f docker/yngdieng-backend/Dockerfile -t ztl8702/yngdieng-backend ./server
