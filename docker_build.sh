59247646c760699d5b4aded2a1a314b0e5d01b2a
Radium Zheng <ztl8702@users.noreply.github.com>
2020-05-16 02:41:09 +0800

    sa

--- docker_build.sh
+++ docker_build.sh
@@ -2,6 +2,9 @@
 
 TAG="${1:-latest}"
 echo $TAG
+
+rm -rf ./.build
+
 # Prod web image
 mkdir -p ./.build/yngdieng-web-prod
 bazel build //web/src:prodapp
@@ -9,12 +12,13 @@ cp -r ./dist/bin/web/src/prodapp ./.build/yngdieng-web-prod/
 cp -r ./docker/yngdieng-web-prod/nginx.conf ./.build/yngdieng-web-prod/
 docker build -f docker/yngdieng-web-prod/Dockerfile -t ztl8702/yngdieng-web-prod:$TAG ./.build/yngdieng-web-prod
 
-# # Dev web image
-# #mkdir -p ./.build/yngdieng-web-dev
-# #bazel build //web/src:devapp_release
-# #cp -r ./dist/bin/web/src/devapp_release ./.build/yngdieng-web-dev/
-# #cp -r ./docker/yngdieng-web-dev/nginx.conf ./.build/yngdieng-web-dev/nginx.conf
-# #docker build -f docker/yngdieng-web-dev/Dockerfile -t ztl8702/yngdieng-web-dev:$TAG ./.build/yngdieng-web-dev
+# Staging web image
+mkdir -p ./.build/yngdieng-web-staging
+bazel build //web/src:staging_app
+cp -r ./dist/bin/web/src/staging_app ./.build/yngdieng-web-staging/
+cp -r ./docker/yngdieng-web-staging/nginx.conf ./.build/yngdieng-web-staging/
+cp -r ./docker/yngdieng-web-staging/robots.txt ./.build/yngdieng-web-staging/
+docker build -f docker/yngdieng-web-staging/Dockerfile -t ztl8702/yngdieng-web-staging:$TAG ./.build/yngdieng-web-staging
 
 # Server image (just one for now)
 docker build -f docker/yngdieng-backend/Dockerfile -t ztl8702/yngdieng-backend:$TAG ./server
