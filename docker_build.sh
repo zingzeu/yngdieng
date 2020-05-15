0f9abe657601ea0d00e12d5f36ec03598822682a
Radium <ztl8702@users.noreply.github.com>
2020-05-16 03:14:22 +0800

    chore(release): Setup dev images (#276)

--- docker_build.sh
+++ docker_build.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
-mkdir -p ./.build/yngdieng-web
+TAG="${1:-latest}"
+echo $TAG
+
+rm -rf ./.build
+
+# Prod web image
+mkdir -p ./.build/yngdieng-web-prod
 bazel build //web/src:prodapp
-cp -r ./dist/bin/web/src/prodapp ./.build/yngdieng-web/
-cp -r ./docker/yngdieng-web/nginx.conf ./.build/yngdieng-web/
+cp -r ./dist/bin/web/src/prodapp ./.build/yngdieng-web-prod/
+cp -r ./docker/yngdieng-web-prod/nginx.conf ./.build/yngdieng-web-prod/
+docker build -f docker/yngdieng-web-prod/Dockerfile -t ztl8702/yngdieng-web-prod:$TAG ./.build/yngdieng-web-prod
 
-docker build -f docker/yngdieng-web/Dockerfile -t ztl8702/yngdieng-web ./.build/yngdieng-web
+# Staging web image
+mkdir -p ./.build/yngdieng-web-staging
+bazel build //web/src:staging_app
+cp -r ./dist/bin/web/src/staging_app ./.build/yngdieng-web-staging/
+cp -r ./docker/yngdieng-web-staging/nginx.conf ./.build/yngdieng-web-staging/
+cp -r ./docker/yngdieng-web-staging/robots.txt ./.build/yngdieng-web-staging/
+docker build -f docker/yngdieng-web-staging/Dockerfile -t ztl8702/yngdieng-web-staging:$TAG ./.build/yngdieng-web-staging
 
-
-docker build -f docker/yngdieng-backend/Dockerfile -t ztl8702/yngdieng-backend ./server
+# Server image (just one for now)
+docker build -f docker/yngdieng-backend/Dockerfile -t ztl8702/yngdieng-backend:$TAG ./server
