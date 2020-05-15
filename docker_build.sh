ccd357cc849919a031e5c1bb5dcaf2925c9fda79
Radium Zheng <ztl8702@users.noreply.github.com>
2020-05-16 02:06:20 +0800

    dc

--- docker_build.sh
+++ docker_build.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
+TAG="${1:-latest}"
+echo $TAG
 # Prod web image
 mkdir -p ./.build/yngdieng-web-prod
 bazel build //web/src:prodapp
 cp -r ./dist/bin/web/src/prodapp ./.build/yngdieng-web-prod/
 cp -r ./docker/yngdieng-web-prod/nginx.conf ./.build/yngdieng-web-prod/
-docker build -f docker/yngdieng-web-prod/Dockerfile -t ztl8702/yngdieng-web-prod ./.build/yngdieng-web-prod
+docker build -f docker/yngdieng-web-prod/Dockerfile -t ztl8702/yngdieng-web-prod:$TAG ./.build/yngdieng-web-prod
 
-# Dev web image
-#mkdir -p ./.build/yngdieng-web-dev
-#bazel build //web/src:devapp_release
-#cp -r ./dist/bin/web/src/devapp_release ./.build/yngdieng-web-dev/
-#cp -r ./docker/yngdieng-web-dev/nginx.conf ./.build/yngdieng-web-dev/nginx.conf
-#docker build -f docker/yngdieng-web-dev/Dockerfile -t ztl8702/yngdieng-web-dev ./.build/yngdieng-web-dev
+# # Dev web image
+# #mkdir -p ./.build/yngdieng-web-dev
+# #bazel build //web/src:devapp_release
+# #cp -r ./dist/bin/web/src/devapp_release ./.build/yngdieng-web-dev/
+# #cp -r ./docker/yngdieng-web-dev/nginx.conf ./.build/yngdieng-web-dev/nginx.conf
+# #docker build -f docker/yngdieng-web-dev/Dockerfile -t ztl8702/yngdieng-web-dev:$TAG ./.build/yngdieng-web-dev
 
 # Server image (just one for now)
-docker build -f docker/yngdieng-backend/Dockerfile -t ztl8702/yngdieng-backend ./server
+docker build -f docker/yngdieng-backend/Dockerfile -t ztl8702/yngdieng-backend:$TAG ./server
