7de3e3bf8b3ca05e145074ac02081e9ee47c74a0
Radium <ztl8702@users.noreply.github.com>
2020-08-02 02:45:16 +0800

    feat(backend): implement GetYngdiengDocument rpc

---
+++ server/backend/Services/YngdiengService.GetYngdiengDocument.cs
@@ -0,0 +1,30 @@
+using System.Threading.Tasks;
+using Yngdieng.Protos;
+using Grpc.Core;
+using System.Linq;
+using Microsoft.Extensions.Logging;
+namespace Yngdieng.Backend.Services
+{
+    public partial class YngdiengService : Yngdieng.Protos.YngdiengService.YngdiengServiceBase
+    {
+
+        public override Task<YngdiengDocument> GetYngdiengDocument(
+            GetYngdiengDocumentRequest request, ServerCallContext context)
+        {
+            _logger.LogInformation("Received YngdiengDocument" + request.Id);
+            return Task.FromResult(
+                SantizeForServing(_indexHolder.GetIndex()
+                                      .YngdiengDocuments.Where(d => d.DocId == request.Id)
+                                      .FirstOrDefault()));
+        }
+
+        private static YngdiengDocument SantizeForServing(YngdiengDocument doc)
+        {
+            var copy = new YngdiengDocument(doc);
+            copy.DocRef = null;
+            copy.IndexingExtension = null;
+            return copy;
+        }
+    }
+
+}
