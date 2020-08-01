782780764473a59fd5e3ceb402fa1b9de1414bff
Radium <ztl8702@users.noreply.github.com>
2020-08-02 05:18:10 +0800

    dsb

--- web/src/app/html-title.ts
+++ web/src/app/html-title.ts
@@ -1,14 +1,12 @@
-
-
-export const APP_NAME = '榕典'
+export const APP_NAME = '榕典';
 
 export function getSearchResultPageTitle(query: string):
     string {
       let trimmed = query.trim();
       return (trimmed == '' ? '搜索' : trimmed) + ' | ' + APP_NAME;
-    }
+};
 
 export function getDetailsPageTitle(word: string): string {
   let trimmed = word.trim();
   return trimmed == '' ? APP_NAME : (trimmed + ' | ' + APP_NAME);
-}
+};
