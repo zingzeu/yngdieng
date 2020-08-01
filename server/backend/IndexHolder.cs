9aa91b8fb1bf370f924d72f93b52fd69a457a972
Radium <ztl8702@users.noreply.github.com>
2020-08-02 02:40:35 +0800

    run clang-format on server/backend/*

--- server/backend/IndexHolder.cs
+++ server/backend/IndexHolder.cs
@@ -2,28 +2,28 @@ using Yngdieng.Protos;
 
 namespace Yngdieng.Backend
 {
-  public interface IIndexHolder
-  {
-      void StoreIndex(YngdiengIndex index);
-      YngdiengIndex GetIndex();
-  }
-
-  public sealed class IndexHolder : IIndexHolder
-  {
-      private YngdiengIndex index = new YngdiengIndex();
-
-      public IndexHolder()
-      {
-      }
-
-      public void StoreIndex(YngdiengIndex index)
-      {
-          this.index = index;
-      }
-
-    public YngdiengIndex GetIndex()
+    public interface IIndexHolder
     {
-      return index;
+        void StoreIndex(YngdiengIndex index);
+        YngdiengIndex GetIndex();
     }
-  }
+
+    public sealed class IndexHolder : IIndexHolder
+    {
+        private YngdiengIndex index = new YngdiengIndex();
+
+        public IndexHolder()
+        {
+        }
+
+        public void StoreIndex(YngdiengIndex index)
+        {
+            this.index = index;
+        }
+
+        public YngdiengIndex GetIndex()
+        {
+            return index;
+        }
+    }
 }
--- server/backend/Services/YngdiengService.GetFengDocument.cs
+++ server/backend/Services/YngdiengService.GetFengDocument.cs
@@ -4,14 +4,15 @@ using Grpc.Core;
 using System.Linq;
 namespace Yngdieng.Backend.Services
 {
-  public partial class YngdiengService : Yngdieng.Protos.YngdiengService.YngdiengServiceBase
-  {
-
-    public override Task<FengDocument> GetFengDocument(GetFengDocumentRequest request, ServerCallContext context)
+    public partial class YngdiengService : Yngdieng.Protos.YngdiengService.YngdiengServiceBase
     {
-      return Task.FromResult(_indexHolder.GetIndex().FengDocuments.Where(f => f.Id == request.Id).FirstOrDefault());
+
+        public override Task<FengDocument> GetFengDocument(GetFengDocumentRequest request,
+                                                           ServerCallContext context)
+        {
+            return Task.FromResult(_indexHolder.GetIndex()
+                                       .FengDocuments.Where(f => f.Id == request.Id)
+                                       .FirstOrDefault());
+        }
     }
-  }
-
-
 }
