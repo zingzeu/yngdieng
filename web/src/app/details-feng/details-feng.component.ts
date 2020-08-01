ef9d63beb0e4b13410464ecfebf151d0d4355efa
Radium <ztl8702@users.noreply.github.com>
2020-08-02 04:48:22 +0800

    aC

--- web/src/app/details-feng/details-feng.component.ts
+++ web/src/app/details-feng/details-feng.component.ts
@@ -28,6 +28,10 @@ export class DetailsFengComponent implements OnInit, OnDestroy {
   private subscription: Subscription;
   private historicalSubscription: Subscription;
 
+  get enableTtsAudio() {
+    return this.environment.showAudioPlayerButtons;
+  }
+
   get showToggleStructured() {
     return this.environment.structuredExplanations.enabled &&
         this.environment.structuredExplanations.showDebugToggle;
