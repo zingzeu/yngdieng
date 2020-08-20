17c128ff2ad9c330df73f2814211d5470e2a9846
Xiangliang <nxl1996@gmail.com>
2020-08-20 13:47:54 -0700

    help page update

--- web/src/app/help/help.component.ts
+++ web/src/app/help/help.component.ts
@@ -1,9 +1,11 @@
 import {Component, OnInit} from '@angular/core';
+// import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-help',
   templateUrl: './help.component.html',
-  styleUrls: ['./help.component.scss']
+  styleUrls: ['./help.component.scss'],
+  preserveWhitespaces: true
 })
 export class HelpComponent implements OnInit {
   constructor() {}
