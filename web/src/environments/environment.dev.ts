404cad03709c8ba344d51d1ea852e828980ff957
Radium <ztl8702@users.noreply.github.com>
2020-08-28 04:06:33 +0800

    save

--- web/src/environments/environment.dev.ts
+++ web/src/environments/environment.dev.ts
@@ -5,7 +5,8 @@ export const YngdiengDevEnvironment: IYngdiengEnvironment = {
   appName: '榕典 (dev)',
   isProduction: false,
   //serverUrl: 'https://yngdieng-api-staging.mindong.asia',
-  serverUrl: 'https://localhost:5001',
+  serverUrl: 'http://localhost:5000',
+  //serverUrl: 'https://localhost:5001',
   structuredExplanations: {
     enabled: true,
     showDebugToggle: true,
