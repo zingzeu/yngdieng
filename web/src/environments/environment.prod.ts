ae75e2e870a5fa346dfa6a871528a655a49025b2
Radium <ztl8702@users.noreply.github.com>
2020-08-28 05:18:53 +0800

    Update environment.prod.ts

--- web/src/environments/environment.prod.ts
+++ web/src/environments/environment.prod.ts
@@ -4,8 +4,7 @@ import {IYngdiengEnvironment} from './environment';
 export const YngdiengProdEnvironment: IYngdiengEnvironment = {
   appName: '榕典',
   isProduction: true,
-  // TODO: use https
-  serverUrl: 'http://api.yngdieng.org',
+  serverUrl: 'https://api.ydict.net',
   structuredExplanations: {
     enabled: true,
     showDebugToggle: false,
