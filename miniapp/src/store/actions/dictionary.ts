84b884c1c7ebbfac6006ae88b32072b7c983a95a
rovinglight <646017375@qq.com>
2020-08-30 16:50:27 +0800

    整理 store

---
+++ miniapp/src/store/actions/dictionary.ts
@@ -0,0 +1,47 @@
+export const search = async (fromIndex = 0, amountToFetch = 10) => {
+  const mockResultList = [
+    {
+      id: '1',
+      title: '我',
+      description: '来源：诸神的游戏 [M]',
+      pinyinRong: 'bung1',
+      rimePosition: '邊春 上平',
+    },
+    {
+      id: '2',
+      title: '崩',
+      description: '来源：诸神的游戏 [M]',
+      pinyinRong: 'bung1',
+      rimePosition: '邊春 上平',
+    },
+    {
+      id: '3',
+      title: '我',
+      description: '来源：诸神的游戏 [M]',
+      pinyinRong: 'bung1',
+      rimePosition: '邊春 上平',
+    },
+    {
+      id: '4',
+      title: '崩',
+      description: '来源：诸神的游戏 [M]',
+      pinyinRong: 'bung1',
+      rimePosition: '邊春 上平',
+    },
+    {
+      id: '5',
+      title: '我',
+      description: '来源：诸神的游戏 [M]',
+      pinyinRong: 'bung1',
+      rimePosition: '邊春 上平',
+    },
+    {
+      id: '6',
+      title: '崩',
+      description: '来源：诸神的游戏 [M]',
+      pinyinRong: 'bung1',
+      rimePosition: '邊春 上平',
+    },
+  ];
+  return mockResultList;
+};
