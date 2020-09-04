c73817406d4b6c7707d1ececa75ec8429a6ceaa7
Radium <ztl8702@users.noreply.github.com>
2020-09-05 06:05:22 +0800

    feat(web): contrib and ciklin details UI (#581)
    Part of #261

---
+++ web/src/app/contrib-explanation/contrib-explanation.component.ts
@@ -0,0 +1,26 @@
+import {Component, OnInit, Input} from '@angular/core';
+import {ContribDocument} from 'yngdieng/shared/documents_pb';
+import {renderExplanation} from '../../yngdieng/explanations';
+
+@Component({
+  selector: 'app-contrib-explanation',
+  templateUrl: './contrib-explanation.component.html',
+  styleUrls: ['./contrib-explanation.component.scss'],
+})
+export class ContribExplanationComponent implements OnInit {
+  @Input('doc') doc: ContribDocument;
+
+  constructor() {}
+
+  get showStructuredExplanation() {
+    return this.doc != null && this.doc.getExplanationStructured() != null;
+  }
+
+  get structuredExplanation() {
+    return renderExplanation(
+      this.doc.getExplanationStructured(),
+      this.doc.getHanzi()
+    );
+  }
+  ngOnInit(): void {}
+}
