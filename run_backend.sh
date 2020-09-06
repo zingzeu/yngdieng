06aaf9b5abe1a2a866aa331606bce2d27d837094
Radium <ztl8702@users.noreply.github.com>
2020-09-06 20:15:11 +0800

    fix commandline args for Yngdieng.Backend

--- run_backend.sh
+++ run_backend.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ASPNETCORE_ENVIRONMENT=Production
 echo Starting Yngdieng.Backend...
-dotnet run -p server/backend/Yngdieng.Backend.csproj -- --IndexFile=../../output/yngdieng_index.bin $@
+dotnet run -p server/backend/Yngdieng.Backend.csproj -- --IndexPath=../../output/ $@
