20cd0596a16e38414fa23095827fbca660af6263
Radium <ztl8702@users.noreply.github.com>
2020-09-21 01:21:02 +0800

    ds

--- web/src/rollup.config.js
+++ web/src/rollup.config.js
@@ -2,10 +2,5 @@ const commonjs = require('@rollup/plugin-commonjs');
 const {nodeResolve} = require('@rollup/plugin-node-resolve');
 
 module.exports = {
-  plugins: [
-    nodeResolve({
-      mainFields: ['browser', 'es2015', 'module', 'jsnext:main', 'main'],
-    }),
-    commonjs(),
-  ],
+  plugins: [nodeResolve(), commonjs()],
 };
