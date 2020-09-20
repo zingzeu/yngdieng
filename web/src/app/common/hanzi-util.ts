550d570c11c0fde49e6a4137a6d2473f387e198b
Radium <ztl8702@users.noreply.github.com>
2020-09-20 23:26:49 +0800

    dev works

--- web/src/app/common/hanzi-util.ts
+++ web/src/app/common/hanzi-util.ts
@@ -1,7 +1,7 @@
 import {Hanzi} from 'yngdieng/shared/documents_pb';
 
 export function hanziToString(h: Hanzi): string {
-  if (h.hasRegular()) {
+  if (h.getRegular() !== '') {
     return h.getRegular();
   }
   return h.getIds();
