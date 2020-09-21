20e8f79a7259db43fbec1d54e61537895c91ccca
rovinglight <646017375@qq.com>
2020-09-21 23:40:19 +0800

    fix: 修复无法返回词表页面的问题

--- miniapp/src/pages/collectionDetail/collectionDetail.tsx
+++ miniapp/src/pages/collectionDetail/collectionDetail.tsx
@@ -124,7 +124,7 @@ const CollectionDetail = () => {
               <View className={styles.listItem} key={word.id}>
                 <WordCard
                   onClick={() =>
-                    Taro.redirectTo({
+                    Taro.navigateTo({
                       url: `${routes.WORD_DETAIL}?id=${word.id}`,
                     })
                   }
