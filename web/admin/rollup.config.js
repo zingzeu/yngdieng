52a33d29f641aba1a43eaf737932dd6dc872f578
Radium <ztl8702@users.noreply.github.com>
2020-10-17 05:20:15 +0800

    save

---
+++ web/admin/rollup.config.js
@@ -0,0 +1,28 @@
+const commonjs = require('@rollup/plugin-commonjs');
+const {nodeResolve} = require('@rollup/plugin-node-resolve');
+
+module.exports = {
+  plugins: [
+    ignoreImport('./../google/api/annotations_pb'),
+    nodeResolve(),
+    commonjs(),
+  ],
+};
+
+function ignoreImport(idToIgnore) {
+  return {
+    name: 'yngdieng-ignore-import',
+    resolveId(source) {
+      if (source === idToIgnore) {
+        return source; // this signals that rollup should not ask other plugins or check the file system to find this id
+      }
+      return null; // other ids should be handled as usually
+    },
+    load(id) {
+      if (id === idToIgnore) {
+        return ''; // source code of the module, which is empty
+      }
+      return null; // other ids should be handled as usually
+    },
+  };
+}
