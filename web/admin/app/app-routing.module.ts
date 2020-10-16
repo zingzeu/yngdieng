8d3a440199cb80b9d129e2138c31b6e495cbb13b
Radium <ztl8702@users.noreply.github.com>
2020-10-17 05:59:24 +0800

    x

--- web/admin/app/app-routing.module.ts
+++ web/admin/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from './home/home.component';
+import {WordsOverviewComponent} from './words-overview/words-overview.component';
 
-const routes: Routes = [{path: '**', component: HomeComponent}];
+const routes: Routes = [
+  {path: 'words', component: WordsOverviewComponent},
+  {path: '**', component: HomeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- web/admin/app/app.module.ts
+++ web/admin/app/app.module.ts
@@ -8,15 +8,24 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import {SidenavContentComponent} from './sidenav-content/sidenav-content.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-
+import {WordsOverviewComponent} from './words-overview/words-overview.component';
+import {MatTableModule} from '@angular/material/table';
+import {MatSortModule} from '@angular/material/sort';
 @NgModule({
-  declarations: [HomeComponent, AppComponent, SidenavContentComponent],
+  declarations: [
+    HomeComponent,
+    AppComponent,
+    SidenavContentComponent,
+    WordsOverviewComponent,
+  ],
   imports: [
     AppRoutingModule,
     BrowserModule,
     CommonModule,
     MaterialModule,
+    MatSortModule,
     BrowserAnimationsModule,
+    MatTableModule,
   ],
   exports: [AppComponent],
   bootstrap: [AppComponent],
