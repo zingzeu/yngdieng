04bd7bdda0ec3b4f3d0472470fafab427ab1287d
Radium <ztl8702@users.noreply.github.com>
2020-10-25 05:23:10 +0800

    update import_csvs.sql

--- server/backend/Db/scripts/import_csvs.sql
+++ server/backend/Db/scripts/import_csvs.sql
@@ -1,11 +1,20 @@
 BEGIN;
 --delete from speakers where true;
+delete from word_audio_clips where true;
+delete from word_list_words where true;
+delete from word_lists where true;
+delete from audio_clips where true;
+delete from speakers where true;
+delete from extensions where true;
+delete from prons where true;
+delete from words where true;
 copy words from '/pg_share/words.csv' delimiter ',' csv header;
 copy prons from '/pg_share/prons.csv' delimiter ',' csv header;
 copy extensions from '/pg_share/extensions.csv' delimiter ',' csv header;
 copy speakers(speaker_id,display_name,accent,location,year_of_birth,gender) from '/pg_share/speakers.csv' delimiter ',' csv header;
 copy audio_clips from '/pg_share/audio_clips.csv' delimiter ',' csv header;
 copy word_lists from '/pg_share/word_lists.csv' delimiter ',' csv header;
+copy word_list_words from '/pg_share/word_list_words.csv' delimiter ',' csv header;
 copy word_audio_clips from '/pg_share/word_audio_clips.csv' delimiter ',' csv header;
 
 COMMIT;
