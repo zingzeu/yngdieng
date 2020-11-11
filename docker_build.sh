6981a97f350398d1dd6ec069796e5d5c8008deef
Radium <ztl8702@users.noreply.github.com>
2020-11-11 23:51:52 +0800

    fix missing opencc data on server

--- docker_build.sh
+++ docker_build.sh
@@ -4,6 +4,12 @@ TAG="${1:-latest}"
 ALIYUN_REPO=registry.cn-hangzhou.aliyuncs.com/zingzeu
 echo $TAG
 
+mkdir -p serverbuild
+cp -rf ./server serverbuild/
+cp -rf ./thirdparty serverbuild/
+
 # Server image (just one for now)
-docker build -f docker/yngdieng-backend/Dockerfile -t ztl8702/yngdieng-backend:$TAG ./server
+docker build -f docker/yngdieng-backend/Dockerfile -t ztl8702/yngdieng-backend:$TAG ./serverbuild
 docker tag ztl8702/yngdieng-backend:$TAG $ALIYUN_REPO/yngdieng-backend:$TAG
+
+rm -rf serverbuild
