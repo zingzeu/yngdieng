f0e2ac02ce26bd1db8dbc35d21c005fa415d68ab
Radium <ztl8702@users.noreply.github.com>
2021-02-11 01:48:56 +0000

    save

--- docker_build.sh
+++ docker_build.sh
@@ -9,7 +9,11 @@ cp -rf ./server serverbuild/
 cp -rf ./thirdparty serverbuild/
 
 # Server image (just one for now)
-docker build -f docker/yngdieng-backend/Dockerfile -t ztl8702/yngdieng-backend:$TAG ./serverbuild
-docker tag ztl8702/yngdieng-backend:$TAG $ALIYUN_REPO/yngdieng-backend:$TAG
+#docker build -f docker/yngdieng-backend/Dockerfile -t ztl8702/yngdieng-backend:$TAG ./serverbuild
+#docker tag ztl8702/yngdieng-backend:$TAG $ALIYUN_REPO/yngdieng-backend:$TAG
+
+# All-in-one image (for Functions)
+docker build -f docker/yngdieng-mono/Dockerfile -t ztl8702/yngdieng-mono:$TAG ./serverbuild 
+docker tag ztl8702/yngdieng-mono:$TAG $ALIYUN_REPO/yngdieng-mono:$TAG
 
 rm -rf serverbuild
