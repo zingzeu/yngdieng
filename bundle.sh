f8a8f585157571fb9bcaf623235ef598f0f49d6a
Radium Zheng <ztl8702@users.noreply.github.com>
2021-12-28 01:06:56 -0500

    save

--- bundle.sh
+++ bundle.sh
@@ -1,2 +1,2 @@
 bazel build --config=release //web/src:prodapp
-cp -rf dist/bin/web/src/prodapp/ dotnet/src/ZingzeuOrg.Yngdieng.Web/wwwroot
+cp -rf dist/bin/web/src/prodapp/* dotnet/src/ZingzeuOrg.Yngdieng.Web/wwwroot/
